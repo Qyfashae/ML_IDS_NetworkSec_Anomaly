f497645f10a6551d306c9e116929235e55b7202c
Felix.T <75949095+Qyfashae@users.noreply.github.com>
2023-09-12 13:12:16 +0100

    Create cap_traffic.py

---
+++ cap_traffic.py
@@ -0,0 +1,18 @@
+import pyshark # Importing pyshark"wireshark"
+import datetime
+import pandas as pd
+
+# Specify the duration for the capture.
+capture_time = 1085 
+
+# Specify the name of the file to output.
+start = datetime.datetime.now() # Starting the time of the network data to capture
+end = start+datetime.datetimedelta(seconds=capture_time) # The end time of the network data capture
+file_name = "net_traffic"+str(start).replace(" ", "T")+"to"+str(end).replace(" ", "T")+".pcap"
+
+# Capture network traffic
+cap = pyshark.LiveCapture(ouput_filename=output_filename)
+cap.sniff(timeout=capture_time)
+
+print("Open the @net_traffic pcap file for manuall investigation.")
+exit(0)
